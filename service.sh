2d7a2942e0a5e895ac347ba3c262ad8010debdc9
Quentin <q.lapointe@gmail.com>
2020-01-07 21:19:50 +0000

    removed support information in module props and moved service.sh at root

---
+++ service.sh
@@ -0,0 +1,21 @@
+#!/system/bin/sh
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
+# This script will be executed in late_start service mode
+
+# restore original apk
+cp "$MODDIR/NfcNci_bak.apk" /system/app/NfcNci/NfcNci.apk
+
+# wait for nfc service to start
+sleep 20
+
+# inject modded apk
+cp "$MODDIR/NfcNci_align.apk" /system/app/NfcNci/NfcNci.apk
+
+# restart nfc service
+killall com.android.nfc
