2427138453caf3b0157415c65b9b60ddfa3732b5
Quentin <q.lapointe@gmail.com>
2020-01-15 23:10:31 +0000

    add checks for android version and replaced apk

--- customize.sh
+++ customize.sh
@@ -3,6 +3,10 @@
 REPLACE="
 /system/app/NfcNci
 "
+APK_PATH="/system/app/NfcNci/NfcNcii.apk"
+
+[ $API -eq 28 ] || abort "! Your version of Android is not compatible."
+[ -f $APK_PATH ] || abort "! Could not find $APK_PATH, your phone may not be compatible with NFC technology."
 
 ui_print "- Backing up original NfcNci.apk"
-cp /system/app/NfcNci/NfcNci.apk $MODPATH/NfcNci_bak.apk
+cp "$APK_PATH" "$MODPATH/NfcNci_bak.apk"
