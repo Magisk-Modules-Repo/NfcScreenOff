948fc95eab63297452b2f7b4762c000102645e08
lapwat <q.lapointe@gmail.com>
2020-05-07 19:02:50 +0200

    add curl/zip fallback binaries

--- customize.sh
+++ customize.sh
@@ -8,45 +8,40 @@ my_grep_prop() {
   sed -n "$REGEX" $FILES 2>/dev/null | head -n 1
 }
 
-ui_print "-- Searching for NFC app in /system/app/ folder..."
+# save device infos
+MANUFACTURER="$(my_grep_prop 'ro\.product\.manufacturer')"
+MODEL="$(my_grep_prop 'ro\.product\.model')"
+DEVICE="$(my_grep_prop 'ro\.product\.device')"
+ROM="$(my_grep_prop 'build\.version')"
+[ -z "$MANUFACTURER" ] && MANUFACTURER="$(my_grep_prop 'ro\.product\.vendor\.manufacturer')"
+[ -z "$MODEL" ] && MODEL="$(my_grep_prop 'ro\.product\.vendor\.model')"
+[ -z "$DEVICE" ] && DEVICE="$(my_grep_prop 'ro\.product\.vendor\.device')"
+echo "MANUFACTURER=$MANUFACTURER\nMODEL=$MODEL\nDEVICE=$DEVICE\nROM=$ROM\nAPK_NAME=$APK_NAME" > "$MODPATH/.env"
 
+# print device infos
+ui_print '-- Device info --'
+ui_print "$(cat $MODPATH/.env)"
+ui_print '-----------------'
+
+ui_print "-- Searching for NFC app in /system/app/ folder..."
 set 'NfcNci' 'NQNfcNci' 'NxpNfcNci'
 for name do
   if [ -d "/system/app/$name" ]; then
     APK_NAME="$name"
   fi
 done
-
 [ -z $APK_NAME ] && abort "!! Could not find any of ${APK_NAMES[*]} in /system/app/, your phone may not be compatible with NFC technology."
+
 ui_print "-- $APK_NAME.apk found!"
-
-# gather device infos
-MANUFACTURER="$(my_grep_prop 'ro\.product\.manufacturer')"
-MODEL="$(my_grep_prop 'ro\.product\.model'       )"
-DEVICE="$(my_grep_prop 'ro\.product\.device'      )"
-ROM="$(my_grep_prop 'build\.version'      )"
-[ -z "$MANUFACTURER" ] && MANUFACTURER="$(my_grep_prop 'ro\.product\.vendor\.manufacturer')"
-[ -z "$MODEL"        ] &&        MODEL="$(my_grep_prop 'ro\.product\.vendor\.model'       )"
-[ -z "$DEVICE"       ] &&       DEVICE="$(my_grep_prop 'ro\.product\.vendor\.device'      )"
-echo "MANUFACTURER=$MANUFACTURER" > "$MODPATH/.env"
-echo "MODEL=$MODEL" >> "$MODPATH/.env"
-echo "DEVICE=$DEVICE" >> "$MODPATH/.env"
-echo "ROM=$ROM" >> "$MODPATH/.env"
-echo "APK_NAME=$APK_NAME" >> "$MODPATH/.env"
-ui_print '-- Device info --'
-ui_print "$(cat $MODPATH/.env)"
-ui_print '-----------------'
-
 APK_PATH="/system/app/$APK_NAME/$APK_NAME.apk"
 REPLACE="
 /system/app/$APK_NAME
 "
-
 mkdir "$MODPATH/$APK_NAME"
 
 # create backup
 ui_print "-- Searching for $APK_NAME.apk backup..."
-if [ -f "/data/adb/modules/NFCScreenOff/${APK_NAME}_bak.apk" ] ; then
+if [ -f "/data/adb/modules/NFCScreenOff/${APK_NAME}_bak.apk" ]; then
   ui_print "-- ${APK_NAME}_bak.apk found! Copying backup to the module update folder."
   cp "/data/adb/modules/NFCScreenOff/${APK_NAME}_bak.apk" "$MODPATH/${APK_NAME}_bak.apk"
 else
@@ -56,9 +51,13 @@ fi
 
 # retrieve modded apk
 ui_print "-- Searching for custom $APK_NAME.apk in extracted files..."
-if [ -f "$MODPATH/${APK_NAME}_align.apk" ] ; then
+if [ -f "$MODPATH/${APK_NAME}_align.apk" ]; then
   ui_print "-- ${APK_NAME}_align.apk found! Nothing to do."
 else
+  # source binaries fallback
+  chmod -R +x "$MODPATH/bin"
+  export PATH="$PATH:$MODPATH/bin"
+ 
   # prepare files
   ui_print "-- ${APK_NAME}_align.apk not found."
   ui_print "-- Zipping $APK_NAME.apk and device's framework"
